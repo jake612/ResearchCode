767f7f7fe6ed5b0ca6f1cc59f3687cdc5e5ef53c
Jacob Ryan <jake612@live.unc.edu>
2019-11-22 13:09:41 -0500

    Finished the framework for uploading and normalizing word vectors

--- UMLS_CUI_Set.py
+++ UMLS_CUI_Set.py
@@ -1,4 +1,6 @@
 import pickle
+import numpy as np
+import time
 
 # Builds dictionary of cui codes and their associated sui codes
 def concept_identifiers(file):
@@ -9,31 +11,53 @@ def concept_identifiers(file):
 			cui = atts[0]
 			sui = atts[5]
 			if cui not in concept_dict:
-				concept_dict[cui] = []
-			concept_dict[cui].append(sui)
+				concept_dict[cui] = set()
+			concept_dict[cui].add(sui)
 	return concept_dict
 
 # Loads vectors of processed word2vec vectors into array of tuples for processing
 # Each tuple is (word/sui, [vector array])
 def load_vectors(file):
-	vectors = []
+	word2index={}
 	with(open(file, "r")) as inp_file:
+		vector_count, dimensions = [int(i) for i in inp_file.readline().strip().split()]
+		matrix = np.zeros((vector_count, dimensions]
+
+		i = 0
 		for line in inp_file:
-			vals = line.split(" ")
-			word = vals[0]
+			vals = line.strip().split()
+			assert len(vals) == dimensions + 1
+			
+			word2index[vals[0]] = i	
+			matrix[i, :] = [float(j) for j in vals[1:]]
+			i+=1
+		return word2index, matrix
 
-			int_vecs = []
-			i = 1
-			# Type conversion of string to int
-			while i < len(vals):
-				int_vecs.append(int(vals[i]))	
-			vectors.append(word, int_vecs)
+# Given a matrix, normalize each row based on the L2-norm
+def normalize_L2(matrix):
+	for i in range(matrix.shape[0]):
+		m[i,:] /= np.linalg.norm(m[i,:])
 
-	return vectors
+
 
 if __name__ == "__main__":
 	# Get the cui code dictionary
+	start_time = time.time()
 	concept_dict = concept_identifiers("MRCONSO.RRF")
+	end_time = time.time()
+	print("CUI dictionary runtime:{}".format(str((end_time-start_time/60))))	
 	
 	# Load vectors from word2vec file
-	vectors = load_vectors("vec.txt")
+	start_time=time.time()
+	word2index, matrix = load_vectors("vec.txt")
+	end_time = time.time()
+	print("Load vectors runtime:{}".format(str((end_time-start_time)/60)))
+
+	# Normalize matrix
+	
+	start_time=time.time()
+	normalize_L2(matrix)
+	end_time = time.time()
+	print("Normalize matrix runtime:{}".format(str((end_time-start_time)/60)))
+	
+	index2word = dict([(i, w) for w, i in word2index.items()])
